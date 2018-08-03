7b4f6655f13e4789658f8eab1c2be5bf33ce22f2
Em Roach <mroach@alteryx.com>
2018-08-02 18:45:49 -0600

    fixed help function typo

--- ayx/Help.py
+++ ayx/Help.py
@@ -22,7 +22,7 @@ class Help:
         > ► <span style="color:grey">*["#1", "#2", "model"]*</span>
         * **Alteryx.installPackages(&nbsp;**<span style="color:blue">"*&lt;package name or list of package names&gt;"&nbsp;*</span>**)**
         *Package(s) will be installed from PyPI. [<span style="font-weight:bold">Note:</span> An internet connection is required. Also, if using an admin install of Alteryx, Alteryx must be opened in admin mode to install packages. Non-admin installs do not have this restriction.*
-        > ```Alteryx.install_packages("tensorflow")```<br/>```Alteryx.install_packages(["keras","theano","gensim"])```
+        > ```Alteryx.installPackages("tensorflow")```<br/>```Alteryx.installPackages(["keras","theano","gensim"])```
 
 
 
