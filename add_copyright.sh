880e19270ab676841d700e708d9e61e0bbb8caf3
Kuo Liu <liukuo99@gmail.com>
2018-10-05 15:01:55 -0600

    Add condition to check if the header of python files are the license info, if not add the license info. This enables the developer to add license info automatically to new python files without messing up with files that already have the license information

--- add_copyright.sh
+++ add_copyright.sh
@@ -2,8 +2,11 @@
 
 
 PY_FILES=$(find ./ -name '*.py')
-for f in $PY_FILES; do 
-    cp copyright.txt temp.txt
-    cat $f >> temp.txt
-    mv temp.txt $f
+
+for f in $PY_FILES; do  
+    if ! grep -q "$(cat copyright.txt)" $f; then
+        cp copyright.txt temp.txt
+        cat $f >> temp.txt
+        mv temp.txt $f
+    fi  
 done
